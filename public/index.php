6791be21e0abb84073e601c6f62a982c22af481e
Bartosz Stepien <bartoszstepien01@interia.pl>
2020-12-22 13:54:45 +0100

    Create interface

--- public/index.php
+++ public/index.php
@@ -1,3 +1,120 @@
-<?php 
-    echo "Hello world!";
-?>
+<!DOCTYPE html>
+<html lang="pl" style="height: 100%;">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <title>Dziennik internetowy</title>
+</head>
+<body class="grey lighten-4" style="height: 100%;">
+    <header style="margin-left: 300px; transition: width 225ms cubic-bezier(0.4, 0, 0.6, 1) 0ms,margin 225ms cubic-bezier(0.4, 0, 0.6, 1) 0ms;">
+        <nav class="top-nav teal">
+                <div class="nav-wrapper">
+                    <a href="#" data-target="nav-mobile" class="top-nav" style="display: block; height: 64px; line-height: 64px; float: left; position: relative; z-index: 1; margin-left: 18px; margin-right: 18px;" onclick="toggleSidebar();">
+                        <i class="material-icons">menu</i>
+                    </a>
+                    <p style="font-size: x-large; margin: auto; /*margin-left: 18px;*/">Dziennik internetowy</a>
+                </div>
+        </nav>
+        <ul class="sidenav sidenav-fixed" style="transform: none; transition: transform 225ms cubic-bezier(0, 0, 0.2, 1) 0ms;">
+            <li>
+                <div class="user-view">
+                    <h6 class="text"><b>Zalogowany jako:</b></h6>
+                    <h6 class="text">Jan Kowalski (nauczyciel)</h6>
+                    <div class="section" style="padding-top: 0;">
+                        <a class="btn-flat teal-text waves-effect" style="padding: 0;">Dane konta</a>
+                        <a class="btn-flat teal-text waves-effect">Wyloguj się</a>
+                    </div>
+                </div>
+                
+            </li>
+            <li><div class="divider"></div></li>
+            <li class="active"><a class="waves-effect waves-teal" href="#!">Wstawianie ocen</a></li>
+            <li><a class="waves-effect waves-teal" href="#!">Sprawdzanie obecności</a></li>
+            <li><a class="waves-effect waves-teal" href="#!">Wstawianie uwag</a></li>
+            <li><a class="waves-effect waves-teal" href="#!">Dodawanie sprawdzianów</a></li>
+            <li><a class="waves-effect waves-teal" href="#!">Dodawanie prac domowych</a></li>
+            <li><a class="waves-effect waves-teal" href="#!">Dodawanie zastępstw</a></li>
+            <li><a class="waves-effect waves-teal" href="#!">Korespondencja</a></li>
+            <li><a class="waves-effect waves-teal" href="#!">Edycja planu lekcji</a></li>
+            <li><div class="divider"></div></li>
+            <li><a class="waves-effect waves-teal" href="#!">Wyświetlanie ocen</a></li>
+            <li><a class="waves-effect waves-teal" href="#!">Wyświetlanie nieobecności</a></li>
+            <li><a class="waves-effect waves-teal" href="#!">Wyświetlanie uwag</a></li>
+            <li><a class="waves-effect waves-teal" href="#!">Podgląd kalendarza</a></li>
+        </ul>
+    </header>
+    <main style="margin-left: 300px; transition: width 225ms cubic-bezier(0.4, 0, 0.6, 1) 0ms,margin 225ms cubic-bezier(0.4, 0, 0.6, 1) 0ms;">
+        <div class="container">
+            <h3>Wstawianie ocen</h3>
+            <div class="card" style="padding: 1rem;">  
+                <form method="post">
+                    <div class="input-field">
+                        <select multiple name="grade[]">
+                            <option value="1">1a</option>
+                            <option value="2">1b</option>
+                            <option value="3">2a</option>
+                            <option value="1">2b</option>
+                            <option value="2">3a</option>
+                            <option value="3">3b</option>
+                        </select>
+                        <label>Klasa</label>
+                    </div>
+                    <div class="input-field">
+                        <select multiple name="subject[]">
+                            <option value="1">Religia</option>
+                            <option value="2">Język polski</option>
+                            <option value="3">Język angielski</option>
+                            <option value="1">Matematyka</option>
+                            <option value="2">Fizyka</option>
+                            <option value="3">Informatyka</option>
+                        </select>
+                        <label>Przedmiot</label>
+                    </div>
+                    <button class="btn waves-effect waves-light" type="submit">Dalej</button>
+                </form>
+            </div>
+            <?php 
+                if($_SERVER["REQUEST_METHOD"] == "POST")
+                {
+                    echo implode(", ", $_POST["grade"]);
+                    echo "<br>";
+                    echo implode(", ", $_POST["subject"]);
+                }
+            ?>
+        </div>
+    </main>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script>
+        let instances;
+
+        let header = document.querySelectorAll('header')[0];
+        let main = document.querySelectorAll('main')[0];
+
+        document.addEventListener('DOMContentLoaded', function() {
+            let elems = document.querySelectorAll('.sidenav');
+            instances = M.Sidenav.init(elems, {
+                onOpenStart: () => {
+                    header.style.marginLeft = "300px"
+                    main.style.marginLeft = '300px'
+                },
+                onCloseStart: () => {
+                    header.style.marginLeft = "0px"
+                    main.style.marginLeft = "0px"
+                }
+            });
+            elems = document.querySelectorAll('select');
+            M.FormSelect.init(elems);
+        });
+
+        function toggleSidebar()
+        {
+            if(instances[0].isOpen)
+                instances[0].close();
+            else 
+                instances[0].open();
+        }
+    </script>
+</body>
+</html>
